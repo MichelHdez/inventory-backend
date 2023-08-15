4a4813ce557c8cba565fe43d459ebeba578b925c
MichelHdez <michel01406@gmail.com>
2023-08-15 13:33:02 -0600

    feat(DEV): actualizacion archivos test

---
+++ src/test/java/com/inventory/junit/AssertEquals.java
@@ -0,0 +1,13 @@
+package com.inventory.junit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AssertEquals {
+
+	@Test
+	public void miTest() {
+		assertEquals(1,1);
+	}
+}
---
+++ src/test/java/com/inventory/junit/AssertNotEquals.java
@@ -0,0 +1,13 @@
+package com.inventory.junit;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AssertNotEquals {
+
+	@Test
+	public void miTest() {
+		assertNotEquals(2, 1);
+	}
+}
